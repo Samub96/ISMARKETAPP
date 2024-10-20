9c0efb90a03f89d6e21127ba21fb72af13fa3c1b
samub96 <samuel.d.barona@gmail.com>
2024-10-20 18:19:41 -0500

    add .gitignore

--- app/src/main/java/com/peludosteam/ismarket/MainActivity.kt
+++ app/src/main/java/com/peludosteam/ismarket/MainActivity.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -96,6 +97,7 @@ fun App() {
         composable("login") { LoginScreen(navController) }
     }
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
